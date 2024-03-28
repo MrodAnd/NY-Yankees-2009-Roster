ca887515610eb9f0b7b8ed9b47142d35c750ba1e
MrodAnd <165328870+MrodAnd@users.noreply.github.com>
2024-03-28 19:34:14 -0400

    Port jsfiddle into  html, cs, and js files

---
+++ scripts.js
@@ -0,0 +1,65 @@
+$('.card').on('click', function () {
+    var player = $(this).data('player')
+
+    $('#column-right').empty()
+
+    if (player === 'Jorge Posada') {
+        $('#column-right').css('background-image', 'url("https://www.si.com/.image/t_share/MTY4MjYxNDY4MTc5NDczNTcz/jorge-posada-ap2jpg.jpg")')
+    }
+
+    if (player === 'Mark Teixeira') {
+        $('#column-right').css('background-image', 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Mark_Teixeira_2009.jpg/943px-Mark_Teixeira_2009.jpg")')
+    }
+
+    if (player === 'Robinson Cano') {
+        $('#column-right').css('background-image', 'url("https://cdn.vox-cdn.com/thumbor/9HlH-ZKT9slzU9OkJigMlk21xB0=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23206312/75655419.jpg")')
+    }
+
+    if (player === 'Derek Jeter') {
+        $('#column-right').css('background-image', 'url("https://i.ebayimg.com/images/g/Bz8AAOSwqu9VFgsC/s-l1600.jpg")')
+    }
+
+    if (player === 'Alex Rodriguez') {
+        $('#column-right').css('background-image', 'url("https://www.savannahnow.com/gcdn/authoring/2009/05/03/NSMN/ghows-GA-7c1203e1-1e62-4eba-9e39-c7a59faca474-1b030ec5.jpeg?width=660&height=834&fit=crop&format=pjpg&auto=webp")')
+    }
+
+    if (player === 'Johnny Damon') {
+        $('#column-right').css('background-image', 'url("https://www.providencejournal.com/gcdn/authoring/2013/08/07/NPRJ/ghows-PJ-a0053925-5b32-497f-b914-1d337d76b16f-771d0fe6.jpeg?width=1200&disable=upscale&format=pjpg&auto=webp")')
+    }
+
+    if (player === 'Melky Cabrera') {
+        $('#column-right').css('background-image', 'url("https://upload.wikimedia.org/wikipedia/commons/d/d6/Melky_Cabrera_2009.jpg")')
+    }
+
+    if (player === 'Nick Swisher') {
+        $('#column-right').css('background-image', 'url("https://cdnph.upi.com/pv/upi/1040d90e74f3d2a9d947db2ed1ca68f9/MLB-2009-Postseason.jpg")')
+    }
+
+    if (player === 'Hideki Matsui') {
+        $('#column-right').css('background-image', 'url("https://upload.wikimedia.org/wikipedia/commons/c/cf/Hideki_Matsui_2009.jpg")')
+    }
+
+    if (player === 'Brett Gardner') {
+        $('#column-right').css('background-image', 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Brett_Gardner_%286093337359%29.jpg/1200px-Brett_Gardner_%286093337359%29.jpg")')
+    }
+
+    if (player === 'CC Sabathia') {
+        $('#column-right').css('background-image', 'url("https://ca-times.brightspotcdn.com/dims4/default/5b606c0/2147483647/strip/true/crop/450x512+0+0/resize/1200x1365!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F70%2Fdc%2Fa38e93ba64e18aa609848a15bff2%2Fsdut-new-york-yankees-cc-sabathia-p-20160828")')
+    }
+
+    if (player === 'A.J. Burnett') {
+        $('#column-right').css('background-image', 'url("https://ca-times.brightspotcdn.com/dims4/default/ad24380/2147483647/strip/true/crop/456x512+0+0/resize/1200x1347!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F52%2Fb3%2F6d119d835c91f9cdec5c3ce8d327%2Fsdut-new-york-yankees-a-j.-burnett-20160829-001")')
+    }
+
+    if (player === 'Andy Pettitte') {
+        $('#column-right').css('background-image', 'url("https://manginphotography.net/wp-content/uploads/2009/12/9N5Q5965-398x575.jpg")')
+    }
+
+    if (player === 'Joba Chamberlain') {
+        $('#column-right').css('background-image', 'url("https://www.si.com/.image/t_share/MTY4MjYyNDg3NDMyMTExMjY5/joba-chamberlainjpg.jpg")')
+    }
+
+    if (player === 'Mariano Rivera') {
+        $('#column-right').css('background-image', 'url("https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2019_29/2940066/190719-mariano-rivera-al-1130.jpg")')
+    }
+})
